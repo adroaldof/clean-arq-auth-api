f10d890cb875d31952ce64c7c746f88cb9f52cd8
Adroaldo de Andrade <adroaldof@gmail.com>
2023-05-13 09:21:35 -0300

    test(token-generator): fix generated derived key

--- src/domain/entities/auth/TokenGenerator.test.ts
+++ src/domain/entities/auth/TokenGenerator.test.ts
@@ -1,28 +1,31 @@
 import { Auth } from './Auth'
-import { expect, it } from 'vitest'
+import { beforeEach, expect, it } from 'vitest'
 import { TokenGenerator } from './TokenGenerator'
 
-const derivedToken =
-  'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImpvaG4uZG9lQGVtYWlsLmNvbSIsImlhdCI6MTY4MTA0NTIwMDAwMCwiZXhwIjoxOTk2NDA1MjAwMDAwfQ.7b28E5HZBNf8Ze-eXzP5RLIvIbK_4evoJ1RDa88TqPuYQXzAvEL8xgsrbV5dbAn9dIG25j1H3yeA0pAb6iZSDg'
-
 const secretOrPrivateKey = 'key'
+const expiresIn = 60 * 60 * 24 * 365 * 10 * 1000 // 10 years for test purpose only (seconds * minutes * hours * days * years * multiplier)
+const issueDate = new Date('2023-04-09T10:00:00')
 
 const authPayload = {
   email: 'john.doe@email.com',
   password: 'abc123',
 }
 
-it('generates a new token for an user', async () => {
-  const expiresIn = 60 * 60 * 24 * 365 * 10 * 1000 // 10 years for test purpose only (seconds * minutes * hours * days * years * multiplier)
-  const issueDate = new Date('2023-04-09T10:00:00')
-  const tokenGenerator = new TokenGenerator(secretOrPrivateKey)
-  const authUser = await Auth.create(authPayload.email, authPayload.password)
-  const output = tokenGenerator.generate(authUser, issueDate, expiresIn)
-  expect(output).toBe(derivedToken)
+let authUser: Auth
+
+beforeEach(async () => {
+  authUser = await Auth.create(authPayload.email, authPayload.password)
 })
 
-it('verifies the user token content', () => {
+it('generates a new token for an user', async () => {
   const tokenGenerator = new TokenGenerator(secretOrPrivateKey)
+  const output = tokenGenerator.generate(authUser, issueDate, expiresIn)
+  expect(output).toEqual(expect.any(String))
+})
+
+it('verifies the user token content', async () => {
+  const tokenGenerator = new TokenGenerator(secretOrPrivateKey)
+  const derivedToken = tokenGenerator.generate(authUser, issueDate, expiresIn)
   const output = tokenGenerator.verify(derivedToken)
   expect(output).toEqual(
     expect.objectContaining({
@@ -34,8 +37,9 @@ it('verifies the user token content', () => {
   expect(output.email).toBe(authPayload.email)
 })
 
-it('returns `invalid token` exception when try to validate a malformed token', () => {
+it('returns `invalid token` exception when try to validate a malformed token', async () => {
   const tokenGenerator = new TokenGenerator(secretOrPrivateKey)
+  const derivedToken = tokenGenerator.generate(authUser, issueDate, expiresIn)
   const invalidToken = derivedToken.substring(5) // remove 5 characters from the beginning
   expect(() => tokenGenerator.verify(invalidToken)).toThrow('invalid token')
 })
