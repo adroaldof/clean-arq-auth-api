bc2d9f8e539f12281a8183950f07da24ebec33aa
Adroaldo de Andrade <adroaldof@gmail.com>
2023-06-12 18:20:57 -0300

    feat(list-users): add use case

--- src/application/ports/AuthRepository.ts
+++ src/application/ports/AuthRepository.ts
@@ -1,6 +1,7 @@
-import { Auth } from '@/entities/auth/Auth';
+import { Auth } from '@/entities/auth/Auth'
 
 export interface AuthRepository {
   save(auth: Auth): Promise<void>
   get(email: string): Promise<Auth | null>
+  list(): Promise<Auth[]>
 }
