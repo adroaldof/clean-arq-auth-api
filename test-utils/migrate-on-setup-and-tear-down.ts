0828794d579cad24673930b2ae71e5a5b12f67a4
Adroaldo de Andrade <adroaldof@gmail.com>
2023-08-09 21:13:07 -0300

    chore(project): update vitest setup and tear down

--- test-utils/migrate-on-setup-and-tear-down.ts
+++ test-utils/migrate-on-setup-and-tear-down.ts
@@ -3,9 +3,11 @@ import { KnexAdapter } from '@/database/KnexAdapter'
 const connection = new KnexAdapter()
 
 export const setup = async () => {
+  if (process.env.NODE_ENV === 'unit') return
   await connection.migrate()
 }
 
 export const teardown = async () => {
+  if (process.env.NODE_ENV === 'unit') return
   await connection.rollback()
 }
